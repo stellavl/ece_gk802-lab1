0c25c9ffdc59085fc29b1a36fe97c7fc6c40411e
Fabio <stella.vlassi01@gmail.com>
2023-03-02 21:00:30 +0200

    solution

--- lab1.py
+++ lab1.py
@@ -1,7 +1,28 @@
 import requests  # εισαγωγή της βιβλιοθήκης
 
-url = input("url:")  # προσδιορισμός του url
+url = input("insert url: ")  # προσδιορισμός του url
 
-with requests.get(url) as response:  # το αντικείμενο response
+#Headers
+with requests.get(url) as response:
     headers = response.headers
-    print(headers)
+    print("Response Headers:\n", headers, "\n")
+
+#Server
+if "Server" in headers:
+    print("Server:",headers["Server"],"\n")
+else:
+    print("No Server\n")
+
+#Cookies   
+if "Set-Cookie" in headers:
+    cookies=response.cookies
+    print("Cookies:\n")
+    i=1
+    for cookie in cookies:
+        print(i,".")
+        print("Name:",cookie.name)
+        print("Value:",cookie.value)
+        print("Expires:",cookie.expires,"\n")
+        i=i+1
+else:
+    print("No Cookies\n")
